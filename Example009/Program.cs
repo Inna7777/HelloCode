15742007c3087185de0cb8a14b103b8eb8551397
Inna7777 <peshkova-inna@mail.ru>
2022-11-30 23:31:34 +0400

    add new  program Base

---
+++ Example009/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите первое число и нажмите Enter");
+string aStr = Console.ReadLine() ??"";
+int a = Convert.ToInt32(aStr);
+Console.WriteLine("Введите второе число и нажмите Enter");
+string bStr = Console.ReadLine() ??"";
+int b = Convert.ToInt32(bStr);
+Console.WriteLine("Введите третье число и нажмите Enter");
+string cStr = Console.ReadLine() ??"";
+int c = Convert.ToInt32(bStr); //Предлагаем ввод чисел
+
+int max = a;
+   if (a>max) max = a;
+   if (b>max) max =b;
+   if (c>max) max =c;
+Console.WriteLine ("max="+max);
