d007101660188b0af5b4c496127b1ae425cd6787
Inna7777 <peshkova-inna@mail.ru>
2022-11-30 23:10:25 +0400

    Add new program

---
+++ Example008/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+string aStr = Console.ReadLine();
+int a = Convert.ToInt32(aStr);
+Console.WriteLine("Введите второе число");
+string bStr = Console.ReadLine();
+int b = Convert.ToInt32(bStr);
+int max = a;
+    if (b == max)
+   {
+    Console.WriteLine($"Числа равны {bStr}={aStr}");
+   }
+    else
+   {
+    if (b > max)
+  { max = b;
+  Console.WriteLine($"большее число: {bStr}, меньшее число : {aStr}" );
+     }
+   
+  else
+   {
+    Console.WriteLine($"большее число: {aStr}, меньшее число : {bStr}");
+   }
+   }
+
+
+
+  
+  
